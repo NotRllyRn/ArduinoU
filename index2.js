ca4b07bc7d1dddfab64e6b2cc25325d4d42bff73
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-01-20 13:00:20 -0800

    hello

--- index2.js
+++ index2.js
@@ -1,17 +1,19 @@
 const express = require('express')
-const hwid = express()
-hwid.use(express.static(__dirname))
-hwid.get("/hwid", function (req, res) {
+const server = express()
+
+server.use(express.static(__dirname))
+server.use(express.json())
+
+server.get("/hwid", function (req, res) {
     res.send('i have your hwid :blush:')
     console.log(req.headers["syn-fingerprint"])
     console.log(req.headers['x-forwarded-for'])
+    console.log(req.ip)
 })
 
-hwid.use(express.json())
-
-hwid.post("/transaction", function(req, res) {
-    if(req.body.type === "validation.webhook")
-        return res.send({id: req.body.id})
+server.post("/transaction", function(req, res) {
+    console.log('hello')
+    if(req.body.type === "validation.webhook") return res.send({id: req.body.id});
 })
 
-hwid.listen(process.env.PORT)
+server.listen(process.env.PORT)
