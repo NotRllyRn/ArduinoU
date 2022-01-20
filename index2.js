94b8a8d8ef0f6b78dea90b67fbe5cbf51b5dc2da
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-01-20 12:34:28 -0800

    please work ip

--- index2.js
+++ index2.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const hwid = express()
 hwid.use(express.static(__dirname))
-hwid.get("/hwid", function(req, res){
+hwid.get("/hwid", function (req, res) {
     res.send('i have your hwid :blush:')
     console.log(req.headers["syn-fingerprint"])
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -11,8 +11,7 @@ hwid.listen(process.env.PORT)
 
 const whitelist = express()
 whitelist.use(express.static(__dirname))
-whitelist.get("/transaction", function(req, res){
+whitelist.get("/transaction", function (req, res) {
     res.send('i have your ip :blush:')
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(ip); // ip address of the user
+    console.log(req.headers, req.headers['x-forwarded-for'], req.connection.remoteAddress)
 })
