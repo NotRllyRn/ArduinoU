8066c26d41061ed822aa953f421fc3ac28d950c9
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-01-20 12:32:50 -0800

    pls

--- index2.js
+++ index2.js
@@ -4,7 +4,8 @@ hwid.use(express.static(__dirname))
 hwid.get("/hwid", function(req, res){
     res.send('i have your hwid :blush:')
     console.log(req.headers["syn-fingerprint"])
-    console.log(req.socket.remoteAddress )
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(ip); // ip address of the user
 })
 hwid.listen(process.env.PORT)
 
@@ -12,5 +13,6 @@ const whitelist = express()
 whitelist.use(express.static(__dirname))
 whitelist.get("/transaction", function(req, res){
     res.send('i have your ip :blush:')
-    console.log(req.socket.remoteAddress )
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(ip); // ip address of the user
 })
