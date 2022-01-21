6fce27e9db0728648240b8c07e9ec1bd7c08fef9
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-01-21 00:44:06 -0800

    test new system

--- index2.js
+++ index2.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
+const crypto = require("crypto");
 const mysql = require('mysql')
 const express = require('express')
-const { Client } = require('discord.js')
+const { Client } = require('discord.js');
+const { connect } = require('http2');
 const client = new Client({
     intents: ['GUILDS', 'DIRECT_MESSAGES', 'GUILD_MESSAGES'],
     partials: ['MESSAGE', 'CHANNEL']
@@ -11,40 +13,78 @@ function getIp(req) {
     let ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim();
     return ip
 }
+function sendChannel(id,msg) {
+    client.channels.fetch(id).send(msg)
+}
 
 let sql = mysql.createConnection({
     host: process.env.DBURL,
     user: process.env.DBUSER,
     password: process.env.DBPASS,
     port: 3306,
-    database: "main"
+    database: 'main'
 })
-
-.connect(function (err) {
+sql.connect(function (err) {
     if (err) throw err;
     console.log('connected')
 });
+
 const server = express()
 server.use(express.static(__dirname));
 server.use(express.json());
 
-server.get("/hwid", function (req, res) {
+server.get('/hwid', function (req, res) {
     res.send('i have your hwid :blush:')
     let ip = getIp(req)
-    console.log(req.headers["syn-fingerprint"])
+    console.log(req.headers['syn-fingerprint'])
     console.log(req.headers['x-forwarded-for'])
     console.log(ip)
 });
 
-server.post("/transaction", function (req, res) {
-    if (!({ '18.209.80.3': true, '54.87.231.232': true }[getIp(req)])) return;
+server.post('/transaction', async function (req, res) {
     let content = req.body
-    if (content.type === "validation.webhook") return res.send({ id: content.id });
+
+    if (!({ '18.209.80.3': true, '54.87.231.232': true }[getIp(req)])) return;
+    if (content.type === 'validation.webhook') return res.send({ id: content.id });
+
     if ((content.type === 'payment.completed') && (content.subject.status.description === 'Complete')) {
-        let tbxid = content.subject.transaction_id
-        let ip = content.subject.customer.ip
-        console.log(content.subject.customer.username)
+        let tbxid = content.subject.transaction_id;
+        let ip = content.subject.customer.ip;
+        let userid = content.subject.customer.username.id;
+        let wkey = crypto.randomBytes(24).toString("hex");
+
+        let pass = false
+        await sql.query(`SELECT * FROM tbxkeys WHERE tbxid = '${tbxid}'`, function (err, data) {
+            if (err) { pass = false; return; }
+            if (data.length === 0) pass = true; else pass = false;
+        });
+        await sql.query(`SELECT * FROM tbxkeys WHERE wkey = '${wkey}'`, function (err, data) {
+            if (err || !pass) { pass = false; return; }
+            if (data.length === 0) pass = true; else pass = false;
+        })
+        if (!pass) return;
+
+        sql.query('INSERT INTO txbkeys SET ?',{
+            tbxid: tbxid,
+            wkey: wkey,
+            ip: ip,
+            whitelist: true,
+            userid: userid
+        }, function(err) {
+            if (err) return; else sendChannel('933071643637612554',
+                '``' + connect.subject.customer.username.username + '`` Whitelisted.\n'
+                + 'Ip: ``' + ip + '``\n'
+                + 'Tbxid: ``' + tbxid + '``\n'
+                + 'UserId: ``' + userid + '``'
+            );
+        });
     }
 });
 
 server.listen(process.env.PORT);
+
+client.on("ready", () => {
+    client.user.setActivity(`for sure`, { type: "LISTENING" });
+});
+
+client.login(DISCORD_TOKEN)
