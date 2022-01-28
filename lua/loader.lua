2dd2536d3849c2a9fb9259968a6bdb8be1fe878b
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-01-27 17:43:33 -0800

    .BODY I FORGOT I HATE THIS AHHJVEBNFG

--- lua/loader.lua
+++ lua/loader.lua
@@ -10,7 +10,7 @@ while true do
 	if not getgenv()["FN2h-jrnFJ2rhrns-iVBU2b"] then
 		break
 	end
-	loadstring(syn.request({ Url = "https://arduinou.herokuapp.com/script", Body = input, Method = "POST" }))()
+	loadstring(syn.request({ Url = "https://arduinou.herokuapp.com/script", Body = input, Method = "POST" }).Body)()
 	if not getgenv()["FN2h-jrnFJ2rhrns-iVBU2b"] then
 		break
 	end
