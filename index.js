cd0dc455c6397904332f20af1e305b070690ae41
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-02-22 19:36:27 -0800

    added a lot of const and stuff

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-require('dotenv').config()
+require('dotenv').config();
 const crypto = require("crypto");
-const mysql = require('mysql')
-const express = require('express')
+const mysql = require('mysql');
+const express = require('express');
 const { Client } = require('discord.js');
 const fs = require('fs');
-const { response } = require('express');
 const client = new Client({
     intents: ['GUILDS', 'DIRECT_MESSAGES', 'GUILD_MESSAGES'],
     partials: ['MESSAGE', 'CHANNEL']
-})
+});
+const sql = mysql.createConnection({
+    host: process.env.DBURL,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    port: 3306,
+    database: 'main'
+});
 
 const DiscordAllowed = {
     '422587947972427777': true
@@ -19,13 +25,12 @@ const botChannels = {
 }
 let dServer;
 const executeScript = '```lua' + `
-key = ''1
+key = ''
 
-loadstring(game:HttpGet('https://arduinou.herokuapp.com/loader', true))()
-` + '```'
+loadstring(game:HttpGet('https://arduinou.herokuapp.com/loader', true))()` + '```'
 
 function hasher(v) {
-    let hased = crypto.createHash('sha3-256').update(v).digest('hex')
+    const hased = crypto.createHash('sha3-256').update(v).digest('hex');
     return hased
 }
 function getIp(req) {
@@ -37,46 +42,39 @@ function fromTebex(req) {
     } else return false;
 }
 
-let sql = mysql.createConnection({
-    host: process.env.DBURL,
-    user: process.env.DBUSER,
-    password: process.env.DBPASS,
-    port: 3306,
-    database: 'main'
-})
 sql.connect(function (err) {
     if (err) throw err;
-    console.log('Mysql Connected.')
+    console.log('Mysql Connected.');
 });
 
-const server = express()
+const server = express();
 server.use(express.static(__dirname + "/site"));
 server.use(express.json());
-let expressCommands = {
+const expressCommands = {
     whitelistCheck: function (req, res) {
-        let content = req.body;
-        let objects = content.object;
-        let wkey = content.key;
-        let check = objects.toString().trim().split('')
-        let pass = ((parseInt(check[1]) % 2) == 0) ? true : false
+        const content = req.body;
+        const objects = content.object;
+        const wkey = content.key;
+        const check = objects.toString().trim().split('');
+        const pass = ((parseInt(check[1]) % 2) == 0) ? true : false;
 
         sql.query('SELECT * FROM whitelist WHERE wkey = ?', [wkey], function (err, result) {
             if (err) return res.send({ Whitelisted: false, object: true }), expressCommands.mainCheck(req, res);
 
             if (result.length === 1) {
-                res.send({ Whitelisted: true, object: pass })
+                res.send({ Whitelisted: true, object: pass });
                 client.channels.cache.get('933054025040031774').send('[``' + wkey + '``] Script executed.');
             } else expressCommands.mainCheck(req, res);
         })
     },
     mainCheck: function (req, res) {
-        let content = req.body;
-        let objects = content.object;
-        let wkey = content.key;
-        let ip = hasher(getIp(req));
-        let hwid = hasher(req.headers['syn-fingerprint']);
-        let check = objects.toString().trim().split('')
-        let pass = ((parseInt(check[1]) % 2) == 0) ? true : false
+        const content = req.body;
+        const objects = content.object;
+        const wkey = content.key;
+        const ip = hasher(getIp(req));
+        const hwid = hasher(req.headers['syn-fingerprint']);
+        const check = objects.toString().trim().split('');
+        const pass = ((parseInt(check[1]) % 2) == 0) ? true : false;
 
         sql.query('SELECT * FROM tbxkeys WHERE wkey = ?', [wkey], function (err, data) {
             if (err) return res.send({ Whitelisted: false, object: true });
@@ -101,15 +99,13 @@ let expressCommands = {
                 }
             } else {
                 res.send({ Whitelisted: false, object: true });
-                client.channels.cache.get('933071691184230400').send('Ip change detected from ``' + data[0].userid + '``')
+                client.channels.cache.get('933071691184230400').send('Ip change detected from ``' + data[0].userid + '``');
             }
         })
     },
     blacklistCheck: function (req, res) {
-        let ip = getIp(req);
-        let hwid = req.headers['syn-fingerprint'];
-        ip = hasher(ip);
-        hwid = hasher(hwid);
+        const ip = hasher(getIp(req));
+        const hwid = hasher(req.headers['syn-fingerprint']);
 
         sql.query('SELECT * FROM blacklisted WHERE ip = ? OR hwid = ?', [ip, hwid], function (err, result) {
             if (err) return res.send({ Whitelisted: false, object: true });
@@ -118,32 +114,32 @@ let expressCommands = {
         })
     },
     checker: function (req, res) {
-        let content = req.body;
-        let objects = content.object;
+        const content = req.body;
+        const objects = content.object;
         let ip = getIp(req);
         let hwid = req.headers['syn-fingerprint'];
 
         if (!ip || !hwid || !content || !objects || !(new String(objects).length == 3)) return res.send({ Whitelisted: false, object: true });
         ip = hasher(ip);
         hwid = hasher(hwid);
-        let check = objects.toString().trim().split('').pop()
+        const check = objects.toString().trim().split('').pop();
 
-        if ((parseInt(check) % 2) == 0) return res.send({ Whitelisted: false, object: false })
+        if ((parseInt(check) % 2) == 0) return res.send({ Whitelisted: false, object: false });
 
-        expressCommands.blacklistCheck(req, res)
+        expressCommands.blacklistCheck(req, res);
     },
     transaction: function (req, res) {
-        let content = req.body;
+        const content = req.body;
 
         if (fromTebex(req)) return;
         if (content.type === 'validation.webhook') return res.send({ id: content.id });
 
         if ((content.type === 'payment.completed') && (content.subject.status.description === 'Complete')) {
-            let productid = content.subject.products[0].id
+            const productid = content.subject.products[0].id;
             if (!productid || !(productid == 4920033)) return res.send({});
-            let tbxid = content.subject.transaction_id;
-            let ip = hasher(content.subject.customer.ip);
-            let userid = content.subject.customer.username.id.toString().trim();
+            const tbxid = content.subject.transaction_id;
+            const ip = hasher(content.subject.customer.ip);
+            const userid = content.subject.customer.username.id.toString().trim();
             let wkey = crypto.randomBytes(12).toString("hex");
 
             function checkkey() {
@@ -163,19 +159,19 @@ let expressCommands = {
                             userid: userid
                         }, function (err) {
                             if (err) return res.send({}); else {
-                                let uuid = content.subject.customer.username.username.toString().trim()
+                                const uuid = content.subject.customer.username.username.toString().trim();
                                 client.channels.cache.get('933071643637612554').send(
                                     '``' + uuid + '`` Whitelisted.\n'
                                     + 'TbxID: ``' + tbxid + '``\n'
                                     + 'UserID: ``' + userid + '``'
                                 );
                                 dServer.members.fetch(userid).then((member) => {
-                                    member.roles.add(dServer.roles.cache.find(r => r.id === '936359030849417278'))
+                                    member.roles.add(dServer.roles.cache.find(r => r.id === '936359030849417278'));
                                     member.send('Key: ``' + wkey + '``').catch(() => {
-                                        client.channels.cache.get('936361136947859516').send('<@' + userid + '> Enable your dms and use ``;getkey``.')
+                                        client.channels.cache.get('936361136947859516').send('<@' + userid + '> Enable your dms and use ``;getkey``.');
                                     });
                                 }).catch(() => {
-                                    client.channels.cache.get('933071691184230400').send('``' + userid + '`` was not in server when buying.\n``' + tbxid + '``')
+                                    client.channels.cache.get('933071691184230400').send('``' + userid + '`` was not in server when buying.\n``' + tbxid + '``');
                                 });
 
                                 res.send({});
@@ -189,7 +185,7 @@ let expressCommands = {
     },
     login: function (req, res) {
         if (fromTebex(req)) return;
-        let uuid = ((req.url || '').toString().split('=').pop().trim()) || '';
+        const uuid = ((req.url || '').toString().split('=').pop().trim()) || '';
 
         dServer.members.fetch(uuid).then(() => {
             res.send({
@@ -229,47 +225,47 @@ let discordCommands = {
             });
             return
         }
-        if (!DiscordAllowed[msg.author.id]) return msg.reply('Unauthorized.')
-        if (!args || args.length < 1) return msg.reply('You need an sql command.')
+        if (!DiscordAllowed[msg.author.id]) return msg.reply('Unauthorized.');
+        if (!args || args.length < 1) return msg.reply('You need an sql command.');
 
         sql.query(args.join(' '), function (err, data) {
             if (err) {
-                msg.reply(err.toString())
+                msg.reply(err.toString());
             } else {
-                msg.reply(JSON.stringify(data, null, ' '))
+                msg.reply(JSON.stringify(data, null, ' '));
             }
         });
     },
     getscript: function (msg) {
         if (!botChannels[msg.channel.id.toString()] && !(msg.channel.type == 'DM')) {
-            msg.delete()
+            msg.delete();
             return msg.channel.send('<@' + msg.author.id + '> Please use bot commands in bot channels.').then(message => {
                 setTimeout(function () {
-                    message.delete()
+                    message.delete();
                 }, 5000);
-            })
+            });
         }
-        let userid = msg.author.id.toString().trim()
+        const userid = msg.author.id.toString().trim();
 
         sql.query('SELECT * FROM tbxkeys WHERE userid = ?', [userid], function (err, data) {
             if (err) return msg.reply('Bot errored.');
             if (data.length === 0) {
-                msg.reply('You are not whitelisted.')
+                msg.reply('You are not whitelisted.');
             } else if (data[0].whitelist.toString() === '1') {
                 msg.author.send(executeScript).catch(() => {
-                    client.channels.cache.get('936361136947859516').send('<@' + msg.author.id.toString().trim() + '> Enable your dms and use ``;getscript``.')
+                    client.channels.cache.get('936361136947859516').send('<@' + msg.author.id.toString().trim() + '> Enable your dms and use ``;getscript``.');
                 });
             } else {
-                msg.reply('You are not whitelisted.')
+                msg.reply('You are not whitelisted.');
             }
         })
     },
     getkey: function (msg) {
         if (!botChannels[msg.channel.id.toString()] && !(msg.channel.type == 'DM')) {
-            msg.delete()
+            msg.delete();
             return msg.channel.send('<@' + msg.author.id + '> Please use bot commands in bot channels.').then(message => {
                 setTimeout(function () {
-                    message.delete()
+                    message.delete();
                 }, 5000);
             })
         }
@@ -277,51 +273,51 @@ let discordCommands = {
             if (err) return msg.reply('Bot errored.');
             if (data.length > 0) {
                 msg.author.send('Key: ``' + data[0].wkey + '``').catch(() => {
-                    client.channels.cache.get('936361136947859516').send('<@' + msg.author.id.toString().trim() + '> Enable your dms and use ``;getkey``.')
+                    client.channels.cache.get('936361136947859516').send('<@' + msg.author.id.toString().trim() + '> Enable your dms and use ``;getkey``.');
                 });
             } else {
-                msg.reply('You are not whitelisted.')
+                msg.reply('You are not whitelisted.');
             }
         })
     },
     getrole: function (msg) {
         if (msg.channel.type == 'DM') return msg.reply('Use this command in the guild.');
         if (!botChannels[msg.channel.id.toString()]) {
-            msg.delete()
+            msg.delete();
             return msg.channel.send('<@' + msg.author.id + '> Please use bot commands in bot channels.').then(message => {
                 setTimeout(function () {
-                    message.delete()
+                    message.delete();
                 }, 5000);
             })
         }
         if (msg.member.roles.cache.some(r => r.id === '936359030849417278')) {
-            msg.reply(`You already have Buyer role.`)
+            msg.reply(`You already have Buyer role.`);
         } else {
             sql.query(`SELECT * FROM tbxkeys WHERE userid = ?`, [msg.author.id.toString().trim()], function (err, data) {
                 if (err) return msg.reply('An error occoured.');
                 if (data.length > 0) {
                     msg.member.roles.add(dServer.roles.cache.find(r => r.id === '936359030849417278'));
-                    msg.reply('Role added.')
+                    msg.reply('Role added.');
                 } else {
-                    msg.reply('You are not whitelisted.')
+                    msg.reply('You are not whitelisted.');
                 }
             })
         }
     },
     ping: function (msg) {
-        msg.reply('no')
+        msg.reply('no');
     }
 }
 client.on("ready", () => {
     client.user.setActivity('you', { type: "LISTENING" });
     dServer = client.guilds.cache.get('936358880907255839');
-    console.log('Discord bot Active.')
+    console.log('Discord bot Active.');
 });
 client.on('messageCreate', (msg) => {
     if (msg.author.bot) return;
-    let content = msg.content
+    const content = msg.content;
     if (content.startsWith(process.env.PREFIX)) {
-        let [name, ...args] = content
+        const [name, ...args] = content
             .trim()
             .substring(process.env.PREFIX.length)
             .split(" ");
@@ -329,11 +325,11 @@ client.on('messageCreate', (msg) => {
             msg,
         ]
         if (args.length > 0) {
-            insert.splice(2, 0, args)
+            insert.splice(2, 0, args);
         }
         if (discordCommands[name]) {
-            discordCommands[name](...insert)
+            discordCommands[name](...insert);
         }
     }
 })
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
