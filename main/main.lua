61afffa0dd8e06511499d17bd2cbbcecceb807b4
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-03-13 15:22:18 -0700

    rushpoint col cool i was adding more stuff but there is still bugs the bugs are the aimbot aim if only visible, its not working. okay cool go fix that

--- main/main.lua
+++ main/main.lua
@@ -1,90 +1,450 @@
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NotRllyRn/Universal-loader/main/UniversalLoader.lua"))(true)
 local library, utility = getVenyx()
 
 local compare_save
 compare_save = function(s1, s2)
-    for name, v in pairs(s1) do
-        if not s2[name] then
-            s2[name] = v
-        elseif s2[name] and type(s2[name]) == 'table' and v and type(v) == 'table' then
-            compare_save(v, s2[name])
-        end
-    end
+	for name, v in pairs(s1) do
+		if not s2[name] then
+			s2[name] = v
+		elseif s2[name] and type(s2[name]) == "table" and v and type(v) == "table" then
+			compare_save(v, s2[name])
+		end
+		s1[name] = s2[name]
+	end
+	for name, v in pairs(s2) do
+		if not s1[name] then
+			s1[name] = v
+		elseif s1[name] and type(s1[name]) == "table" and v and type(v) == "table" then
+			compare_save(v, s1[name])
+		end
+	end
 end
 
 local loadSettings = function(settings)
-    if isfolder('Arduino') then
-        local input
-        local s = pcall(function()
-            input = JSONDecode(readfile('Arduino/saved.json'))
-        end)
-        if s then
-            compare_save(settings, input)
-            settings = input
-        else
-            local input = JSONEncode(settings)
-            writefile('Arduino/saved.json', input)
-        end
-    else
-        makefolder('Arduino')
-        local input = JSONEncode(settings)
-        writefile('Arduino/saved.json', input)
-    end
+	if isfolder("Arduino") then
+		local inputt
+		local s = pcall(function()
+			inputt = JSONDecode(readfile("Arduino/saved.json"))
+		end)
+		if s then
+			compare_save(settings, inputt)
+		else
+			local inputt = JSONEncode(settings)
+			writefile("Arduino/saved.json", inputt)
+		end
+	else
+		makefolder("Arduino")
+		local inputt = JSONEncode(settings)
+		writefile("Arduino/saved.json", inputt)
+	end
 end
 
 local saveSettings = function(settings)
-    if isfolder('Arduino') and isfile('Arduino/saved.json') then
-        local input
-        local s = pcall(function()
-            input = JSONEncode(settings)
-        end)
-        if s then
-            writefile('Arduino/saved.json', input)
-        else
-            return false
-        end
-    else
-        makefolder('Arduino')
-        local input = JSONEncode(settings)
-        writefile('Arduino/saved.json', input)
-    end
+	if isfolder("Arduino") and isfile("Arduino/saved.json") then
+		local inputt
+		local s = pcall(function()
+			inputt = JSONEncode(settings)
+		end)
+		if s then
+			writefile("Arduino/saved.json", inputt)
+		else
+			return false
+		end
+	else
+		makefolder("Arduino")
+		local inputt = JSONEncode(settings)
+		writefile("Arduino/saved.json", inputt)
+	end
 end
 
+local games_scripts = {
+	["5993942214"] = {
+		check = function()
+			return (workspace:Has('MapFolder') and workspace.MapFolder:Has('Players') and localPlayer:Has('PermanentTeam'))
+		end,
+		main = function(window, settings)
+			local PlayersFolder = workspace.MapFolder.Players
+			local local_table = {
+				inGame = false,
+				team = localPlayer.PermanentTeam.Value,
+			}
+
+			local checkGame = function()
+				local found = false
+				for _, plr in ipairs(PlayersFolder:GetChildren()) do
+					if plr.Name == localPlayer.Name then
+						found = true
+						break
+					end
+				end
+				local_table.inGame = found
+				return local_table.inGame
+			end
+			checkGame()
+
+			local Settings = {
+				ESP_SETTINGS = {
+					tracers = false,
+					box = true,
+					aim = "humanoid",
+					on = true,
+					colors = {
+						sameTeam = { 0, 255, 0 },
+						otherTeam = { 255, 0, 0 },
+						aimed = {255, 255, 255}
+					},
+					keybind = 'E',
+					overide = false,
+				},
+				AIMBOT_SETTINGS = {
+					smooth = 10,
+					on = false,
+					distance = 250,
+					aim = "head",
+					keybind = 'T',
+					showaim = true,
+					visible = false,
+					aim_setting = 'closest to player',
+				},
+			}
+			compare_save(Settings, settings.GAMES["5993942214"].SETTINGS)
+			settings.GAMES["5993942214"].SETTINGS = Settings
+			Settings = settings.GAMES["5993942214"].SETTINGS
+
+			local game_table = {}
+			local esp_run = function()
+				checkGame()
+				for NAME, _ in pairs(game_table) do
+					if not (players:Has(NAME)) then
+						game_table[NAME].line:Remove()
+						game_table[NAME].box:Remove()
+						game_table[NAME] = nil
+					elseif not (PlayersFolder:Has(NAME)) then
+						game_table[NAME].inGame = false
+						game_table[NAME].line.Visible = false
+						game_table[NAME].box.Visible = false
+					end
+				end
+				for _, plr in ipairs(PlayersFolder:GetChildren()) do
+					local plr_Char = plr
+					local plr = players:Has(plr.Name)
+					if
+						plr
+						and plr_Char
+						and plr_Char:Has("HumanoidRootPart")
+						and plr_Char:Has("Head")
+						and not (plr.Name == localPlayer.Name)
+					then
+						if not game_table[plr.Name] then
+							game_table[plr.Name] = {
+								line = Drawing.new("Line"),
+								box = Drawing.new("Quad"),
+								targets = {
+									humanoid = plr_Char.HumanoidRootPart,
+									head = plr_Char.Head,
+								},
+								team = plr.PermanentTeam.Value,
+								inGame = true,
+								aiming = false,
+							}
+						else
+							game_table[plr.Name].targets = {
+								humanoid = plr_Char.HumanoidRootPart,
+								head = plr_Char.Head,
+							}
+							game_table[plr.Name].team = plr.PermanentTeam.Value
+							game_table[plr.Name].inGame = true
+						end
+
+						local line = game_table[plr.Name].line
+						local box = game_table[plr.Name].box
+						local target = game_table[plr.Name].targets[Settings.ESP_SETTINGS.aim]
+						local torso = game_table[plr.Name].targets.humanoid
+
+						if game_table[plr.Name].team == local_table.team then
+							line.Color = Color3.new(table.unpack(Settings.ESP_SETTINGS.colors.sameTeam))
+							box.Color = Color3.new(table.unpack(Settings.ESP_SETTINGS.colors.sameTeam))
+						else
+							line.Color = Color3.new(table.unpack(Settings.ESP_SETTINGS.colors.otherTeam))
+							box.Color = Color3.new(table.unpack(Settings.ESP_SETTINGS.colors.otherTeam))
+						end
+						if (Settings.AIMBOT_SETTINGS.on and Settings.AIMBOT_SETTINGS.showaim and game_table[plr.Name].aiming) then
+							line.Color = Color3.new(table.unpack(Settings.ESP_SETTINGS.colors.aimed))
+							box.Color = Color3.new(table.unpack(Settings.ESP_SETTINGS.colors.aimed))
+						end
+
+						line.From = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2)
+						line.Thickness = 2
+						box.Thickness = 2
+						box.Filled = false
+
+						local point = getPoint(target)
+						if point then
+							line.To = point
+						end
+
+						if getPoint(torso) then
+							local TL = getPoint(torso.CFrame * CFrame.new(-3, 3, 0).p, true)
+							local TR = getPoint(torso.CFrame * CFrame.new(3, 3, 0).p, true)
+							local BL = getPoint(torso.CFrame * CFrame.new(-3, -3, 0).p, true)
+							local BR = getPoint(torso.CFrame * CFrame.new(3, -3, 0).p, true)
+
+							box.PointA = TL
+							box.PointB = TR
+							box.PointC = BR
+							box.PointD = BL
+						end
+
+						if (local_table.inGame or Settings.ESP_SETTINGS.overide) and game_table[plr.Name].inGame and Settings.ESP_SETTINGS.on then
+							if point and Settings.ESP_SETTINGS.tracers then
+								line.Visible = true
+							else
+								line.Visible = false
+							end
+							if getPoint(torso) and Settings.ESP_SETTINGS.box then
+								box.Visible = true
+							else
+								box.Visible = false
+							end
+						else
+							line.Visible = false
+							box.Visible = false
+						end
+					elseif game_table[plr.Name] then
+						game_table[plr.Name].inGame = false
+						game_table[plr.Name].line.Visible = false
+						game_table[plr.Name].box.Visible = false
+					end
+				end
+			end
+
+			local aimbot_run = function()
+				local distance = Settings.AIMBOT_SETTINGS.distance
+				if Settings.AIMBOT_SETTINGS.aim_setting == 'closest to player' then
+					distance = 1e9
+				end
+
+				local aimAT;
+
+				for _, stuff in pairs(game_table) do
+					if not (local_table.team == stuff.team) and (stuff.inGame) then
+						local target = stuff.targets[Settings.AIMBOT_SETTINGS.aim]
+
+						local point, on = getPoint(target)
+						if point and ((Settings.AIMBOT_SETTINGS.visible and on) or (not Settings.AIMBOT_SETTINGS.visible)) then
+							local dis = (Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2) - point).Magnitude
+							if Settings.AIMBOT_SETTINGS.aim_setting == 'closest to player' and local_table.inGame then
+								local dis = (target.Position - humanoidRP.Position).Magnitude
+							end
+							if dis < distance then
+								aimAT = target
+								distance = dis
+							end
+						end
+					end
+				end
+
+				if aimAT and Settings.AIMBOT_SETTINGS.on then
+					for name, stuff in pairs(game_table) do
+						stuff.aiming = false
+						if name == aimAT.Parent.Name then
+							stuff.aiming = true
+						end
+					end
+
+					tweenService:Create(camera, TweenInfo.new(Settings.AIMBOT_SETTINGS.smooth / 100), {
+						CFrame = CFrame.new(camera.CFrame.Position, aimAT.Position),
+					}):Play()
+				end
+			end
+
+			cWrap(function()
+				renderS:Connect(function()
+					cWrap(function()
+						esp_run()
+					end)
+					cWrap(function()
+						aimbot_run()
+					end)
+				end)
+			end)
+
+			local page = window:addPage(settings.GAMES["5993942214"].NAME, 5012544693) do
+				local ESP = page:addSection('ESP') do
+					local tog = ESP:addToggle('Esp toggle', Settings.ESP_SETTINGS.on, function(v)
+						Settings.ESP_SETTINGS.on = v
+					end)
+					ESP:addToggle('Esp Overide', Settings.ESP_SETTINGS.overide, function(v)
+						Settings.ESP_SETTINGS.overide = v
+					end)
+					ESP:addToggle('Tracers', Settings.ESP_SETTINGS.tracers, function(v)
+						Settings.ESP_SETTINGS.tracers = v
+					end)
+					ESP:addToggle('Boxes', Settings.ESP_SETTINGS.box, function(v)
+						Settings.ESP_SETTINGS.box = v
+					end)
+					ESP:addDropdown('Aim at', {'head', 'humanoid'}, function(v)
+						Settings.ESP_SETTINGS.aim = v
+					end)
+					cWrap(function()
+						wait(1)
+						ESP:addKeybind('Esp toggle keybind', Enum.KeyCode[Settings.ESP_SETTINGS.keybind], function()
+							Settings.ESP_SETTINGS.on = not Settings.ESP_SETTINGS.on
+							ESP:updateToggle(tog, 'Esp toggle', Settings.ESP_SETTINGS.on)
+						end, function(key)
+							Settings.ESP_SETTINGS.keybind = tostring(key.KeyCode):split(".")[3]
+						end)
+						ESP:Resize()
+					end)
+				end
+				local COLORS = page:addSection('ESP Colors') do
+					COLORS:addColorPicker('Same team', Color3.new(table.unpack(Settings.ESP_SETTINGS.colors.sameTeam)), function(c)
+						Settings.ESP_SETTINGS.colors.sameTeam = {c.R, c.G, c.B}
+					end)
+					COLORS:addColorPicker('Opponent team', Color3.new(table.unpack(Settings.ESP_SETTINGS.colors.otherTeam)), function(c)
+						Settings.ESP_SETTINGS.colors.otherTeam = {c.R, c.G, c.B}
+					end)
+					COLORS:addColorPicker('Aimbot target color', Color3.new(table.unpack(Settings.ESP_SETTINGS.colors.aimed)), function(c)
+						Settings.ESP_SETTINGS.colors.aimed = {c.R, c.G, c.B}
+					end)
+				end
+				local AIMBOT = page:addSection('AIMBOT') do
+					local tog = AIMBOT:addToggle('Aimbot toggle', Settings.AIMBOT_SETTINGS.on, function(v)
+						Settings.AIMBOT_SETTINGS.on = v
+					end)
+					AIMBOT:addKeybind('Aimbot toggle keybind', Enum.KeyCode[Settings.AIMBOT_SETTINGS.keybind], function()
+						Settings.AIMBOT_SETTINGS.on = not Settings.AIMBOT_SETTINGS.on
+						AIMBOT:updateToggle(tog, 'Aimbot toggle', Settings.AIMBOT_SETTINGS.on)
+					end, function(key)
+						Settings.AIMBOT_SETTINGS.keybind = tostring(key.KeyCode):split(".")[3]
+					end)
+					AIMBOT:addToggle('Show aiming at', Settings.AIMBOT_SETTINGS.showaim, function(v)
+						Settings.AIMBOT_SETTINGS.showaim = v
+					end)
+					AIMBOT:addToggle('Player has to be Visible', Settings.AIMBOT_SETTINGS.visible, function(v)
+						Settings.AIMBOT_SETTINGS.visible = v
+					end)
+					AIMBOT:addDropdown('Aim at', {'head', 'humanoid'}, function(v)
+						Settings.AIMBOT_SETTINGS.aim = v
+					end)
+					AIMBOT:addDropdown('Aim method', {'closest to player', 'closest to mouse'}, function(v)
+						Settings.AIMBOT_SETTINGS.aim_setting = v
+					end)
+					AIMBOT:addSlider('Smoothness', Settings.AIMBOT_SETTINGS.smooth, 1, 100, function(v)
+						Settings.AIMBOT_SETTINGS.smooth = v
+					end)
+					AIMBOT:addSlider('Distance', Settings.AIMBOT_SETTINGS.distance, 1, 1000, function(v)
+						Settings.AIMBOT_SETTINGS.distance = v
+					end)
+				end
+				window:SelectPage(window.pages[1], true)
+			end
+
+			for i,v in pairs(getgc(true)) do
+				if v and type(v) == 'table' then
+					local s,e = pcall(function()
+						return (v.Remove and v.Visible and v.Color and v.Transparency and v.ZIndex)
+					end)
+					if s and e then
+						v.Visible = false
+					end
+				end
+			end
+		end,
+	},
+}
+
 local Settings = {
-    UI_SETTINGS = {
-        UI_POS = UDim2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y / 2),
-        COLORS = {
-            Background = Color3.new(0.0941176, 0.0941176, 0.0941176),
-            Glow = Color3.new(0, 0, 0),
-            Accent = Color3.new(0.0392157, 0.0392157, 0.0392157),
-            LightContrast = Color3.new(0.0784314, 0.0784314, 0.0784314),
-            DarkContrast = Color3.new(0.054902, 0.054902, 0.054902),  
-            TextColor = Color3.new(1, 1, 1),
-        },
-        GAMES_UI = {}
-    },
-    GAMES = {}
+	UI_SETTINGS = {
+		UI_POS = { 0, camera.ViewportSize.X / 2, 0, camera.ViewportSize.Y / 2 },
+		COLORS = {
+			Background = { 0.0941176, 0.0941176, 0.0941176 },
+			Glow = { 0, 0, 0 },
+			Accent = { 0.0392157, 0.0392157, 0.0392157 },
+			LightContrast = { 0.0784314, 0.0784314, 0.0784314 },
+			DarkContrast = { 0.054902, 0.054902, 0.054902 },
+			TextColor = { 1, 1, 1 },
+		},
+		OPEN_CLOSE = "RightShift",
+	},
+	GAMES = {
+		["5993942214"] = {
+			NAME = "Rush Point",
+			SETTINGS = {},
+		},
+	},
+	AUTOFARM = {
+		ON = false,
+		DATA = nil,
+	},
 }
 
 loadSettings(Settings)
 
 local load_ui = function(settings)
-    local Window = library.new('Arduino') do
-        for theme, color3 in pairs(settings.UI_SETTINGS.COLORS) do
-            Window:setTheme(theme, color3)
-        end
-    end
+	local window = library.new("Arduino")
+	do
+		for theme, color3 in pairs(settings.UI_SETTINGS.COLORS) do
+			window:setTheme(theme, Color3.new(table.unpack(color3)))
+		end
+	end
 
-    Window.container.Main.Position = settings.UI_SETTINGS.UI_POS
+	utility:DraggingEnded(function()
+		settings.UI_SETTINGS.UI_POS = {
+			0,
+			window.container.Main.Position.X.Offset,
+			0,
+			window.container.Main.Position.Y.Offset,
+		}
+	end)
 
-    utility:DraggingEnded(function()
-        settings.UI_SETTINGS.UI_POS = Window.container.Main.Position
-    end)
+	return window
+end
 
-    return Window
+local finalize_ui = function(window, settings)
+	local set = window:addPage("Ui Settings", 5012544693)
+	local colors = set:addSection("Colors")
+
+	for theme, color in pairs(settings.UI_SETTINGS.COLORS) do
+		local color = Color3.new(table.unpack(color))
+
+		colors:addColorPicker(theme, color, function(color3)
+			window:setTheme(theme, color3)
+			settings.UI_SETTINGS.COLORS[theme] = { color3.R, color3.G, color3.B }
+		end)
+	end
+
+	local ui_s = set:addSection("Miscellaneous")
+	ui_s:addKeybind("Toggle UI", Enum.KeyCode[settings.UI_SETTINGS.OPEN_CLOSE], function()
+		settings.UI_SETTINGS.UI_POS = {
+			0,
+			window.container.Main.Position.X.Offset,
+			0,
+			window.container.Main.Position.Y.Offset,
+		}
+		window:toggle()
+	end, function(key)
+		settings.UI_SETTINGS.OPEN_CLOSE = tostring(key.KeyCode):split(".")[3]
+	end)
+	ui_s:addButton("Save Settings", function()
+		saveSettings(settings)
+	end)
+
+	window.container.Main.Position = UDim2.new(table.unpack(settings.UI_SETTINGS.UI_POS))
+
+	onLeave(function()
+		saveSettings(settings)
+	end)
 end
 
 local Arduino = load_ui(Settings) do
-
+	for _, ta in pairs(games_scripts) do
+		if ta.check() then
+			cWrap(function()
+				ta.main(Arduino, Settings)
+			end)
+			break
+		end
+	end
+	finalize_ui(Arduino, Settings)
 end
