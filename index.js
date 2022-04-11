789019a77d3d28117f9beed7b61611042c1777ae
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-04-10 23:14:43 -0700

    bruh synapse hwid is lowercase im such a duimbass

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function fromTebex(req) {
     } else return false;
 }
 function getHWID(req) {
-    const headers = ["Syn-Fingerprint", "Krnl-Hwid"];
+    const headers = ["syn-fingerprint", "Krnl-Hwid"];
     for (let i = 0; i < headers.length; i++) {
         if (req.headers[headers[i]]) {
             return req.headers[headers[i]];
